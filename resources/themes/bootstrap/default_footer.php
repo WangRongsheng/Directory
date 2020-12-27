59dbcc997e0aef93058d8c59d8f27aa03845557a
WangRongsheng <fujingshusheng@gmail.com>
2020-12-27 20:29:43 +0800

    init project

---
+++ resources/themes/bootstrap/default_footer.php
@@ -0,0 +1,5 @@
+<footer class="container">
+  <div class="footer">
+ © 2020-<?php echo date("Y")?> <a href="https://github.com/WangRongsheng/Directory" target="_blank">王荣胜</a> . All rights reserved.
+   </div>
+</footer>
